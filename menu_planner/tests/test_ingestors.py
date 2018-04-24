9e5929b7481b057cd1ab1460523510870990cda6
tandrade <nos.tamaramos@gmail.com>
2018-04-23 22:28:20 -0500

    Add in test case with url

---
+++ menu_planner/tests/test_ingestors.py
@@ -0,0 +1,7 @@
+
+
+class IngestorTestCases(TestCase):
+
+    def test_url_recipe_parsed(self):
+        test_url = "https://communitytable.parade.com/606801/alisonashton/carrot-salad-with-tahini-chickpeas-and-pistachios/"
+        test_data = "/data/carrot_salad__community_table"
