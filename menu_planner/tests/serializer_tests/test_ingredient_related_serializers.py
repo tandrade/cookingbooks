edfbd53eb1ccf4492bb2aba6775b3124d7863664
tandrade <nos.tamaramos@gmail.com>
2018-12-29 13:45:53 -0600

    Added test, requirements.txt

--- menu_planner/tests/serializer_tests/test_ingredient_related_serializers.py
+++ menu_planner/tests/serializer_tests/test_ingredient_related_serializers.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-from menu_planner.models import Recipe, Ingredient
-from menu_planner.serializers import RecipeIngredientItemSerializer
+from menu_planner.models import Ingredient, Recipe
+from menu_planner.serializers import IngredientSerializer, RecipeIngredientItemSerializer
 
 
 class RecipeIngredientItemSerializerTest(TestCase):
@@ -41,3 +41,28 @@ class RecipeIngredientItemSerializerTest(TestCase):
 
         serialized = RecipeIngredientItemSerializer(data=data)
         self.assertTrue(serialized.is_valid())
+
+
+class IngredientSerializerTest(TestCase):
+
+    def test_different_capitalization(self):
+        i1 = IngredientSerializer(data={"name": "onion"})
+        self.assertTrue(i1.is_valid())
+        i1.save()
+        self.assertEqual(len(Ingredient.objects.all()), 1)
+
+        i2 = IngredientSerializer(data={"name": "Onion"})
+        self.assertTrue(i2.is_valid())
+        i2.save()
+        self.assertEqual(len(Ingredient.objects.all()), 1)
+
+    def test_plurals_dont_create_multiple(self):
+        i1 = IngredientSerializer(data={"name": "chickpea"})
+        self.assertTrue(i1.is_valid())
+        i1.save()
+        self.assertEqual(len(Ingredient.objects.all()), 1)
+
+        i2 = IngredientSerializer(data={"name": "chickpeas"})
+        self.assertTrue(i2.is_valid())
+        i2.save()
+        self.assertEqual(len(Ingredient.objects.all()), 1)
